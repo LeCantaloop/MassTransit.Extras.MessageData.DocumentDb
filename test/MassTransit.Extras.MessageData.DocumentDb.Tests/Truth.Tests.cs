c90aa4c6b4c0a1ef813d3211dd4a0e1c8e675391
Matt Kotsenas <Matt.Kotsenas@gmail.com>
2016-05-13 14:12:48 -0700

    Add Test project
    - Add Tests project with a single truth test

---
+++ test/MassTransit.Extras.MessageData.DocumentDb.Tests/Truth.Tests.cs
@@ -0,0 +1,9 @@
+﻿using Machine.Specifications;
+
+namespace MassTransit.Extras.MessageData.DocumentDb.Tests
+{
+    public class when
+    {
+        It should_be_true = () => true.ShouldBeTrue();
+    }
+}
